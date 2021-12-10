ae1b254b90445a52f5a7ceebac887c2733a58fb4
ss11mik <ss11mik@protonmail.com>
2021-12-10 20:51:44 +0100

    Cleanup

--- model/main.cpp
+++ model/main.cpp
@@ -31,17 +31,11 @@ int cuttingTrottlePercent = 0;
 
 int main(int args, char* argv[]) {
 
-
-    //Operation cutting(4, 1), z_profile(1.5, 2), bending(2, 2), enclosing(2, 2), cutting2(1, 1), completation(3, 1), planting(2, 1);
-    //Storage storage;
-   // Operation qa(1, 1), expedition(30, 1);
-
     Print("Zacatek simulace\n");
 
    // SetOutput("model.out");
     Init(0, SIM_TIME_SPAN);
     (new CuttingOp)->Activate();
-  // (new mStat)->Activate();
 
     Run();
 
